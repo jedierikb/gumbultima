d036736eee8c4b5a7737becec224c806c21d8050
Erik Blankinship <erikb@mediamods.com>
2021-07-09 10:46:10 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-// const reverse = require( 'lodash/reverse' );
-import * as _ from "https://jspm.dev/lodash@4.17.21"
-
 export default function Button(props) {
-    const huh = _.reverse( 'oh no'.split('') ).join('');
-    return React.createElement("div", null, huh);
+    return React.createElement("div", null, 'huh');
 }
